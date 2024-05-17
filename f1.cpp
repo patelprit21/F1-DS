bb0c7b4ffd5c831c1f87e3644a991d353890c044
Prit <patelprit985@gmail.com>
2024-05-17 17:00:12 -0400

    Took input of date and destination for once from a user and initialized required variables

--- f1.cpp
+++ f1.cpp
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+#define MAX_INPUT_LENGTH 30
 
 typedef struct {
     char* destination;
@@ -7,5 +10,27 @@ typedef struct {
 }FlightInfo;
 
 int main(){
+    FlightInfo flightInfo[10];
+    char* destination = (char*)malloc(MAX_INPUT_LENGTH + 1);
+    char* date = (char*)malloc(MAX_INPUT_LENGTH + 1);
+
+    // take input of date and destination
+    printf("Hello User, Enter the destinations and dates for  each flights as specified below.\n\n");
+
+
+    // take destination input
+    if(fgets(destination, MAX_INPUT_LENGTH, stdin) == NULL){
+        printf("Error in reading destination\n");
+    }
+    destination[strlen(destination)-1] ='\0';
+
+    // // take date input
+    if(fgets(date, MAX_INPUT_LENGTH, stdin) == NULL){
+        printf("Error in reading date\n");
+    }
+    date[strlen(date)-1] ='\0';
+    
+
+
     return 0;
 }
