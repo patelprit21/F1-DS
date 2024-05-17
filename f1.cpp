b555ff905d420413907487f894a3b8e8bb3dff2a
Prit <patelprit985@gmail.com>
2024-05-17 17:32:24 -0400

    Added fillFlightInfo function and called it

--- f1.cpp
+++ f1.cpp
@@ -3,6 +3,7 @@
 #include<string.h>
 
 #define MAX_INPUT_LENGTH 30
+#define MAX_FLIGHTS 10
 
 typedef struct {
     char* destination;
@@ -13,18 +14,18 @@ typedef struct {
 void fillFlightInfo(FlightInfo* flightInfo, char* destination, char* date);
 
 int main(){
-    FlightInfo flightInfo[10];
+    FlightInfo flightInfo[MAX_FLIGHTS];
     char* destination = (char*)malloc(MAX_INPUT_LENGTH + 1);
     char* date = (char*)malloc(MAX_INPUT_LENGTH + 1);
 
     // take input of date and destination
     printf("Hello User, Enter the destinations and dates for  each flights as specified below.\n\n");
 
-    int flights = 10;
+    int flights = MAX_FLIGHTS;
 
     while(flights--){
 
-        printf("--------------- Flight %d ---------------\n", 10- flights);
+        printf("--------------- Flight %d ---------------\n", 10 - flights);
 
         // take destination input
         printf("Destination: ");
@@ -40,9 +41,22 @@ int main(){
         }
         date[strlen(date)-1] ='\0';
 
-
+        fillFlightInfo(&flightInfo[10-flights], destination, date);
+        printf("%s %s\n", flightInfo[10-flights].destination, flightInfo[10-flights].date);
     }
 
-
+    
     return 0;
+}
+
+void fillFlightInfo(FlightInfo * FlightInfo, char* destination, char* date){
+    
+    // allocate memory
+    FlightInfo->destination = (char*)malloc(strlen(destination)+1);
+    FlightInfo->date = (char*)malloc(strlen(date)+1);
+
+    // set fields
+    strcpy(FlightInfo->destination, destination);
+    strcpy(FlightInfo->date, date);
+
 }
